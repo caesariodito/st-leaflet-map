e06b95785c60f0ccbe0b2a8160fd759e6f5d406d
Caesario Dito <caesariodito2014@gmail.com>
2022-08-25 00:17:58 +0700

    leaflet with layer control + circle markers

---
+++ data/aktivitas.js
@@ -0,0 +1,37 @@
+var aktivitasJSON = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [107.8363037109375, -6.560931820902541],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [108.83056640625, -7.476857400769451],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [110.3741455078125, -7.01366792756663],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [111.884765625, -8.059229627200192],
+            },
+        },
+    ],
+}
---
+++ data/relawan.js
@@ -0,0 +1,117 @@
+var relawanJSON = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [106.951904296875, -6.217012327817175],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [106.72119140625, -6.195168442930952],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [106.820068359375, -6.266157747202609],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [106.907958984375, -6.337137394988534],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [106.710205078125, -6.331677767365065],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [110.3466796875, -7.759980241585301],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [110.21484375, -7.732765062729807],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [110.3961181640625, -7.667441482726056],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [109.259033203125, -7.400600345742086],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [109.13818359375, -7.389705401310166],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [107.611083984375, -6.964596817535289],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [107.6275634765625, -7.01366792756663],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [107.5341796875, -6.986406835762532],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+                type: 'Point',
+                coordinates: [111.42883300781249, -6.964596817535289],
+            },
+        },
+    ],
+};
